6076e82639a07528c16aa31da8e0ca7a8c75d8fd
ijaz35 <ijaz.hossain1@gmail.com>
2022-10-10 21:56:42 +0600

    module-38

---
+++ setInterval.js
@@ -0,0 +1,6 @@
+console.log(11111);
+console.log(22222);
+console.log(33333);
+console.log(44444);
+setInterval(() => { console.log('tik tik tik') }, 1000);
+console.log(55555);
---
+++ timeout.js
@@ -0,0 +1,10 @@
+console.log('I am first');
+console.log('I am second');
+console.log('I am third');
+setTimeout(function () {
+    console.log('Coding in VS code');
+}, 5000);
+setTimeout(() => {
+    console.log('Exploring MDN document');
+}, 3000);
+console.log('I am fourth');
